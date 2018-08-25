6e637b8a5bed2624dd91cdbc1a66244f08888efa
sandybegn <sandeepsekhar555@gmail.com>
2018-08-25 19:51:49 +0800

    test

--- src/TestRunner.java
+++ src/TestRunner.java
@@ -3,7 +3,9 @@ public class TestRunner {
 
 	public static void main(String[] args) {
 		
-System.out.println("hello");
+		System.out.println("hello");
+		String s ="sandeep";
+		System.out.println(s);
 	}
 
 }

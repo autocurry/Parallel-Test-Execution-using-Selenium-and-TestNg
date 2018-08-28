1669d148e46130bfe6f18114731426b9771cf987
sandybegn <sandeepsekhar555@gmail.com>
2018-08-28 22:59:34 +0800

    parallel execution without browser

---
+++ src/TestngPack/ParallelExecution.java
@@ -0,0 +1,42 @@
+package TestngPack;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+
+public class ParallelExecution {
+	
+		   
+	   
+	   @Test
+	   public void firstest() {
+	      System.out.println("Inside firsttest()");
+	      
+	   }
+		   
+	   @Test
+	   public void secondtest() {	
+	      System.out.println("Inside secondtest()");     
+	       
+	   }
+	   
+	   @Test
+	   public void thirdtest() {
+	      System.out.println("Inside thirdtest()");
+	      
+	   }
+	   
+	   @Test
+	   public void fouthtest() {
+	      System.out.println("Inside fourthtest()");
+	      
+	   }
+	}
+
